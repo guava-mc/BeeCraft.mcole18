10d968b4783d94517c87a31c0868126f9d7e5566
Mason Cole <mcole18@asu.edu>
2018-11-13 14:24:59 -0800

    Initial commit with tools and templates set-up

---
+++ src/test/DPTestSuite.java
@@ -0,0 +1,24 @@
+/**
+ * File:   DPTestSuite.java
+ * Date:   Nov 13, 2018
+ * 
+ */
+
+package test;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+/**
+ * Description: TODO
+ * 
+ * @author: mcole18
+ * @version 1.0
+ *
+ */
+@RunWith(Suite.class)
+@SuiteClasses({})
+public class DPTestSuite {
+
+}
